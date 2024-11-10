835e9804270561d869546a970f32fddb648067ad
eskelt <diegogpr3@gmail.com>
2024-11-10 11:08:41 +0100

    Springboot project created. Added dependencies and gitignore update

---
+++ src/main/java/org/coffe/atradius/CoffeAssesmentApplication.java
@@ -0,0 +1,11 @@
+package org.coffe.atradius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeAssesmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeAssesmentApplication.class, args);
+    }
+}
